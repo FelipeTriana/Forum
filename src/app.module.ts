9288bd2ed565f435ca319b3703c1eb32bdaa17db
FelipeTriana <felipe.trianag@udea.edu.co>
2021-07-26 10:06:46 -0500

    Add new ormconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,7 @@ import { ResponsesModule } from './responses/responses.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '192.168.30.62',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'publications',
-      entities: ['dist/**/*.entity{.ts,.js}'], //Expresion regular para encontrar todas las entidades
-      synchronize: false, //No autosincronizado(migraciones)
-      retryDelay: 3000, // En cada intento de conexion demora 3 ms
-      retryAttempts: 10, //10 intentos de conexion
-    }),
+    TypeOrmModule.forRoot(),
     PublicationsModule,
     VerifytokenModule,
     ResponsesModule,
